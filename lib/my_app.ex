9ecd777c6bbd85a7107deaf6ce7988eeea7196f9
Ethan Trawick <ethan.trawick@me.com>
2025-02-02 22:34:35 -0500

    :sparkles: Refactor isolates side effects and new pure func

--- lib/my_app.ex
+++ lib/my_app.ex
@@ -1,48 +1,71 @@
 defmodule MyApp do
   use GenServer
+  require Logger
+
   @moduledoc """
   Main entry point for the application.
   """
 
   @spec start_link(any()) :: :ignore | {:error, any()} | {:ok, pid()}
   def start_link(filename) do
-    GenServer.start_link(__MODULE__, filename)
+    GenServer.start_link(__MODULE__, filename, name: __MODULE__)
   end
 
+  @impl true
   def init(filename) do
-    {:ok, watcher_pid} = FileSystem.start_link(dirs: ["../"], file: filename)
+    {:ok, watcher_pid} = FileSystem.start_link(dirs: ["../"])
     FileSystem.subscribe(watcher_pid)
     initial_content = File.read!(filename)
-    {:ok, %{watcher_pid: watcher_pid, filename: filename, last_content: initial_content}}
+
+    {:ok,
+     %{
+       filename: filename,
+       last_content: initial_content,
+       last_line: nil
+     }}
   end
 
-  def handle_info({:file_event, watcher_pid, {path, events}},
-    %{watcher_pid: watcher_pid, filename: filename, last_content: last_content} = state) do
-
-    if Path.basename(path) == filename and :modified in events do
+  @impl true
+  def handle_info({:file_event, _watcher_pid, {path, events}}, state) do
+    if file_modified?(path, events, state) do
+      # Use the "path" from the message instead of state.path, which doesn't exist.
       new_content = File.read!(path)
-      new_lines = find_new_lines(last_content, new_content)
-
-      Enum.each(new_lines, fn line ->
-        IO.puts("New line: #{line}")
-      end)
-
-      {:noreply, %{state | last_content: new_content}}
+      new_lines = extract_new_lines(state.last_content, new_content)
+      last_line = extract_last_line(new_lines, state.last_line)
+      notify_new_lines(new_lines)
+      new_state = %{state | last_content: new_content, last_line: last_line}
+      {:noreply, new_state}
     else
       {:noreply, state}
     end
   end
 
   def handle_info({:file_event, watcher_pid, :stop}, %{watcher_pid: watcher_pid} = state) do
-    # Your own logic when monitor stop
     IO.puts("Stopping wisp watcher")
     {:noreply, state}
   end
 
-  defp find_new_lines(old_content, new_content) do
-    old_lines = String.split(old_content, "\n")
-    new_lines = String.split(new_content, "\n")
+  def file_modified?(path, events, state) do
+    Path.basename(path) == state.filename and :modified in events
+  end
+
+  defp extract_new_lines(old_content, new_content) do
+    old_lines = String.split(old_content, "\n", trim: true)
+    new_lines = String.split(new_content, "\n", trim: true)
     new_lines -- old_lines
   end
 
+  defp extract_last_line([], last_line), do: last_line
+  defp extract_last_line(lines, _), do: List.last(lines)
+
+  defp notify_new_lines(new_lines) do
+    Enum.each(new_lines, fn line ->
+      IO.puts("New line: #{line}")
+    end)
+  end
+
+  @impl true
+  def terminate(reason, _) do
+    Logger.error("Terminating due to: #{inspect(reason)}")
+  end
 end
--- test/my_app_test.exs
+++ test/my_app_test.exs
@@ -1,25 +1,99 @@
 defmodule MyAppTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  import ExUnit.CaptureIO
   doctest MyApp
 
-  test "reads file" do
-    assert {:ok, pid} = MyApp.start_link("minecraft_fabric.log")
-    assert is_pid(pid)
+  alias MyApp
+
+  describe "file_modified?/3" do
+    test "returns true if filename matches and events include :modified" do
+      state = %{filename: "testfile.log"}
+      assert MyApp.file_modified?("/path/to/testfile.log", [:modified, :other], state)
+    end
+
+    test "returns false if filename does not match" do
+      state = %{filename: "other.log"}
+      refute MyApp.file_modified?("/path/to/testfile.log", [:modified], state)
+    end
+
+    test "returns false if events do not include :modified" do
+      state = %{filename: "testfile.log"}
+      refute MyApp.file_modified?("/path/to/testfile.log", [:created], state)
+    end
   end
 
-  test "does not read file" do
-    assert {:error, _reason} = MyApp.start_link("non_existent_file.log")
-  end
+  describe "handle_info/2 with file events" do
+    setup do
+      tmp_dir = System.tmp_dir!()
+      test_file = Path.join(tmp_dir, "testfile_#{:erlang.unique_integer([:positive])}.log")
 
-  test "streams new lines" do
-    tmp_file = "tmp_stream_test.log"
-    File.write!(tmp_file, "new line\n")
+      initial_content = "line1\nline2"
+      File.write!(test_file, initial_content)
 
-    {:ok, pid} = MyApp.start_link(tmp_file)
-    assert is_pid(pid)
-    File.write!(tmp_file, "2nd line\n", [:append])
+      # Return test_file and initial_content as part of the context.
+      on_exit(fn -> File.rm(test_file) end)
+      {:ok, test_file: test_file, initial_content: initial_content}
+    end
 
-    :timer.sleep(50)
-    assert_receive {:line, "2nd line\n"}
+    test "detects new line added to file", %{
+      test_file: test_file,
+      initial_content: initial_content
+    } do
+      state = %{
+        filename: Path.basename(test_file),
+        last_content: initial_content,
+        last_line: nil
+      }
+
+      new_content = initial_content <> "\nline3"
+      File.write!(test_file, new_content)
+
+      output =
+        capture_io(fn ->
+          MyApp.handle_info({:file_event, self(), {test_file, [:modified]}}, state)
+        end)
+
+      assert output =~ "New line: line3"
+    end
+
+    test "ignores event if file does not match state.filename", %{
+      test_file: test_file,
+      initial_content: initial_content
+    } do
+      state = %{
+        filename: "some_other_file.log",
+        last_content: initial_content,
+        last_line: nil
+      }
+
+      output =
+        capture_io(fn ->
+          MyApp.handle_info({:file_event, self(), {test_file, [:modified]}}, state)
+        end)
+
+      refute output =~ "New line"
+    end
+
+    test "ignores event when events do not include :modified", %{
+      test_file: test_file,
+      initial_content: initial_content
+    } do
+      state = %{
+        filename: Path.basename(test_file),
+        last_content: initial_content,
+        last_line: nil
+      }
+
+      # Update file but send a different event type (e.g. :created)
+      new_content = initial_content <> "\nline3"
+      File.write!(test_file, new_content)
+
+      output =
+        capture_io(fn ->
+          MyApp.handle_info({:file_event, self(), {test_file, [:created]}}, state)
+        end)
+
+      refute output =~ "New line: line3"
+    end
   end
 end
